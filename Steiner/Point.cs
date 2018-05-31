8f0f75ac121a857214c71a06b6406a83ac2e90c4
tkaczmarz <kaczmarz.tomasz@gmail.com>
2018-05-31 14:15:20 +0200

    Init

---
+++ Steiner/Point.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Steiner
+{
+    public class Point
+    {
+        public double x;
+        public double y;
+        public double Value { get { return x + y; } }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static List<Point> GetList(params double[] coords)
+        {
+            if (coords.Length % 2 != 0)
+                return new List<Point>();
+
+            List<Point> list = new List<Point>();
+            for (int i = 0; i < coords.Length - 1; i += 2)
+            {
+                list.Add(new Point(coords[i], coords[i + 1]));
+            }
+
+            return list;
+        }
+
+        public bool LessThan(Point q)
+        {
+            if (q.Value < Value || (q.Value == Value && x < q.x))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Point Merge(Point q)
+        {
+            double minX = x < q.x ? x : q.x;
+            double minY = y < q.y ? y : q.y;
+
+            return new Point(minX, minY);
+        }
+
+        public double Cost(Point q)
+        {
+            return Math.Abs(x - q.x) + Math.Abs(y - q.y);
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+}
---
+++ Steiner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Steiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = Point.GetList(0, 0, 1, 2, 2, 4, 3, 1, 3, 2, 4, 2, 4, 4, 5, 1);
+
+            foreach (Point p in points)
+                Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
